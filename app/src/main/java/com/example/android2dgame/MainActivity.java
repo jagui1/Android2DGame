48313e6bee2eeb531ce1c83f6398a952442213f6
jagui1 <agui1@umbc.edu>
2020-09-24 21:08:26 -0400

    Adds game loop

--- app/src/main/java/com/example/android2dgame/MainActivity.java
+++ app/src/main/java/com/example/android2dgame/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.android2dgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * MainActivity is the entry point to our application
+ */
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Set window to fullscreen (will hide status bar)
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new Game(this));
     }
 }
